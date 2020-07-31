dbbcf64946df072c16f5391d9bb8990210628941
Bobby Hargett <brhargett+github@gmail.com>
2020-07-31 11:42:58 -0400

    Merge

--- app/src/main/java/com/brh/ipe/MainActivity.kt
+++ app/src/main/java/com/brh/ipe/MainActivity.kt
@@ -6,19 +6,15 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.brh.ipe.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
+    lateinit var binding: ActivityMainBinding
 
     override fun setContentView(view: View?) {
-        println("Wahooooooooo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+        binding = ActivityMainBinding.bind(view!!)
         super.setContentView(view)
     }
 
@@ -26,11 +22,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         super.onPostCreate(savedInstanceState, persistentState)
         setSupportActionBar(findViewById(R.id.toolbar))
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+        binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
@@ -52,9 +44,59 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
         }
     }
 
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.nav_host_fragment)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+    private fun setupGreetMsgEdit() {
+        binding.tilMsg.addOnEditTextAttachedListener {
+            it.editText!!.setOnFocusChangeListener {v, hasFocus ->
+                if (hasFocus && v.id == R.id.ed_msg) {
+                    focusOnGreetMsgEdit()
+                } else {
+                    endGreetMsgEditing()
+                }
+            }
+            false
+        }
     }
+
+    private fun focusOnGreetMsgEdit() {
+        if (binding.tilMsg.endIconDrawable == null) {
+            with(binding.tilMsg) {
+                startIconDrawable = resources.getDrawable(R.drawable.ic_baseline_check_24, null)
+                endIconDrawable = resources.getDrawable(R.drawable.ic_baseline_delete_24, null)
+                endIconMode = TextInputLayout.END_ICON_CUSTOM
+                /*setEndIconTintList(ColorStateList.valueOf(resources.getColor(R.color.colorPrimary, null)))
+                setStartIconTintList(ColorStateList.valueOf(resources.getColor(R.color.colorPrimary, null)))*/
+
+                setEndIconOnClickListener {
+                    endGreetMsgEditing()
+                }
+
+                /*setStartIconOnClickListener {
+                    val newGreetMsg = binding.edMsg.text.toString()
+                    if (!newGreetMsg.isNullOrBlank()) {
+                        binding.progressBar.root.visibility = View.VISIBLE
+                        with(viewModel) {
+                            updateOptions(GreetMsg(newGreetMsg, greetMsg.default_projectid))
+                        }
+                    }
+                }*/
+            }
+
+        }
+    }
+
+    private fun endGreetMsgEditing(revertText: Boolean = true) {
+        /*binding.progressBar.root.visibility = View.GONE
+        with(binding.tilMsg) {
+            error = null
+            endIconMode = TextInputLayout.END_ICON_NONE
+            if (revertText)
+                binding.edMsg.setText(viewModel.greetMsg.message, TextView.BufferType.NORMAL)
+            binding.edMsg.focusSearch(View.FOCUS_DOWN)?.requestFocus()
+            SoftKeyboardUtils.HideSoftKeyboard(requireActivity())
+            startIconDrawable = null
+        }*/
+    }
+
+
+
 }
